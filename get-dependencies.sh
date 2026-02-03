ac38ec12b2e6a589e8d3c396e03886a862d15a99
Link4Electronics <link4electronics@gmail.com>
2026-02-03 12:46:33 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -42,7 +42,26 @@ cmake . \
     -GNinja \
     -DNON_PORTABLE=On \
     -DCMAKE_INSTALL_PREFIX=/usr \
-    -DCMAKE_C_FLAGS="-Wno-incompatible-pointer-types -Wno-int-conversion"
+    -DCMAKE_C_FLAGS="-Wno-incompatible-pointer-types -Wno-int-conversion -Wno-changes-meaning"
 cmake --build build --config Release
 cmake --build build --config Release --target GenerateO2R
 cmake --install build
+
+#install -dm755 /usr/bin/
+#install -Dm755 "Spaghettify" /usr/bin/
+# Create Directories
+#  install -dm755 "${pkgdir}/${SHIP_PREFIX}" "${pkgdir}/usr/bin/"
+  # Main executable & assets to /opt
+#  cp -r build/yamls build/meta "${pkgdir}/${SHIP_PREFIX}"
+#  install -m755 build/Spaghettify "${pkgdir}/${SHIP_PREFIX}"
+#  install -m644 -t "${pkgdir}/${SHIP_PREFIX}" \
+#        build/config.yml \
+#        build/spaghetti.o2r \
+#        "${srcdir}/SDL_GameControllerDB/gamecontrollerdb.txt"
+  # Link executable to /usr/bin, add to desktop entry & icons
+#  ln -s "${SHIP_PREFIX}/Spaghettify" "${pkgdir}/usr/bin/Spaghettify"
+#  install -Dm644 "${srcdir}/spaghettikart.desktop" -t "${pkgdir}/usr/share/applications"
+#  install -Dm644 icon.png "${pkgdir}/usr/share/pixmaps/spaghettikart.png"
+  # Licenses (HarbourMasters libraries are MIT, game engine + port source code is nonfree)
+#  install -Dm644 "libultraship/LICENSE" "${pkgdir}/usr/share/licenses/spaghettikart/libultraship-LICENSE"
+#  install -Dm644 "torch/LICENSE" "${pkgdir}/usr/share/licenses/spaghettikart/torch-LICENSE"
