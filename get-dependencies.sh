4b99617ce858053627508b017fff8377d4e4ed59
Link4Electronics <link4electronics@gmail.com>
2026-02-03 12:11:57 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -34,11 +34,15 @@ git clone --recursive --depth 1 "$REPO" ./SpaghettiKart
 echo "$VERSION" > ~/version
 
 cd ./SpaghettiKart
+patch -Np1 -i "../spaghettikart-cmake-flags.patch"
+export CFLAGS="${CFLAGS/-Werror=format-security/}"
+export CXXFLAGS="${CXXFLAGS/-Werror=format-security/}"
 cmake . \
     -Bbuild \
     -GNinja \
     -DNON_PORTABLE=On \
-    -DCMAKE_INSTALL_PREFIX=/usr
-cmake --build build --config Release
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -Wno
+cmake --build build --config Release $NINJAFLAGS
 cmake --build build --config Release --target GenerateO2R
 cmake --install build
