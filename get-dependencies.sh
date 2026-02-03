fbb18643eba5239c99d50fc9c94229945f9a71b2
Link4Electronics <link4electronics@gmail.com>
2026-02-03 13:10:37 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -35,15 +35,12 @@ echo "$VERSION" > ~/version
 
 cd ./SpaghettiKart
 #patch -Np1 -i "../spaghettikart-cmake-flags.patch"
-#export CFLAGS+="-Werror=format-security"
-#export CXXFLAGS+="-Werror=format-security"
 cmake . \
     -Bbuild \
     -GNinja \
     -DNON_PORTABLE=On \
     -DCMAKE_INSTALL_PREFIX=/usr \
-    -DCMAKE_C_FLAGS="-Wno-error"
-    #-DCMAKE_C_FLAGS="-Wno-incompatible-pointer-types -Wno-int-conversion -Wno-changes-meaning"
+    -DCMAKE_C_FLAGS="-Wno-incompatible-pointer-types -Wno-int-conversion -Wno-changes-meaning"
 cmake --build build --config Release
 cmake --build build --config Release --target GenerateO2R
 cmake --install build
