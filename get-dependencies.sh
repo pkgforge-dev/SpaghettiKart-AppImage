6df6ee96e43f0d16f7ce9ecb31fe86d675555c22
Link4Electronics <link4electronics@gmail.com>
2026-02-03 11:45:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,39 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm tinyxml2
+pacman -Syu --noconfirm \
+    cmake         \
+    fmt           \
+    libzip        \
+    ninja         \
+    nlohmann-json \
+    sdl2          \
+    sdl2_net      \
+    spdlog        \
+    tinyxml2      \
+    zenity
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of SpaghettiKart..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/HarbourMasters/SpaghettiKart"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./SpaghettiKart
+echo "$VERSION" > ~/version
+
+cd ./SpaghettiKart
+cmake . \
+    -Bbuild \
+    -GNinja \
+    -DNON_PORTABLE=On \
+    -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build build --config Release $NINJAFLAGS
+cmake --build build --config Release --target GenerateO2R
+cmake --install build
