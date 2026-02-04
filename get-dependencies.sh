520767a6f9f7f015e89544e74b954a4682d4e0b7
Link4Electronics <link4electronics@gmail.com>
2026-02-04 00:30:05 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -44,25 +44,11 @@ cmake . \
 cmake --build build --config Release
 cmake --build build --config Release --target GenerateO2R
 
-
 install -dm755 /usr/bin/
-#install -Dm755 "Spaghettify" /usr/bin/
-# Create Directories
-#  install -dm755 "${pkgdir}/${SHIP_PREFIX}" "/usr/bin/"
-  # Main executable & assets to /opt
 install -m755 build/Spaghettify /usr/bin
 install -m644 build/config.yml -t /usr/bin
 install -m644 build/spaghetti.o2r -t /usr/bin
-cp -r build/yamls /usr/bin
-cp -r build/meta /usr/bin
-
-#install -m644 -t "/usr/bin" \
-#        build/config.yml \
-#        build/spaghetti.o2r
-        #"SDL_GameControllerDB/gamecontrollerdb.txt"
-#install -Dm644 yamls -t "/usr/bin"
-# Link executable to /usr/bin, add to desktop entry & icons
-#  ln -s "${SHIP_PREFIX}/Spaghettify" "${pkgdir}/usr/bin/Spaghettify"
+cp -r build/yamls build/meta /usr/bin
 sed -i 's/^Icon=icon$/Icon=SpaghettiKart/' SpaghettiKart.desktop
 install -Dm644 "SpaghettiKart.desktop" -t "/usr/share/applications"
 install -Dm644 icon.png "/usr/share/pixmaps/SpaghettiKart.png"
