807afeb534acf9a641645cafa04845a38e071787
Link4Electronics <link4electronics@gmail.com>
2026-02-04 00:44:23 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -38,17 +38,19 @@ patch -Np1 -i "../spaghettikart-cmake-flags.patch"
 cmake . \
     -Bbuild \
     -GNinja \
-    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_INSTALL_PREFIX=/usr/spaghettikart \
     -DCMAKE_C_FLAGS="-Wno-incompatible-pointer-types -Wno-int-conversion -Wno-changes-meaning"
     #-DNON_PORTABLE=On \
 cmake --build build --config Release
 cmake --build build --config Release --target GenerateO2R
 
 install -dm755 /usr/bin/
-install -m755 build/Spaghettify /usr/bin
-install -m644 build/config.yml -t /usr/bin
-install -m644 build/spaghetti.o2r -t /usr/bin
-cp -r build/yamls build/meta /usr/bin
+install -dm755 /usr/spaghettikart
+install -m755 build/Spaghettify /usr/spaghettikart
+ln -s "usr/spaghettikart/Spaghettify" "/usr/bin/Spaghettify"
+install -m644 build/config.yml -t /usr/spaghettikart
+install -m644 build/spaghetti.o2r -t /usr/spaghettikart
+cp -r build/yamls build/meta /usr/spaghettikart
 sed -i 's/^Icon=icon$/Icon=SpaghettiKart/' SpaghettiKart.desktop
 install -Dm644 "SpaghettiKart.desktop" -t "/usr/share/applications"
 install -Dm644 icon.png "/usr/share/pixmaps/SpaghettiKart.png"
