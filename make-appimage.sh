1fba5057c9df861c967bdabc24ea491413365ac7
Link4Electronics <link4electronics@gmail.com>
2026-02-04 04:06:29 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/Spaghettify /usr/bin/zenity /usr/share/spaghettikart
-mv /usr/share/spaghettikart/spaghetti.o2r /usr/share/spaghettikart/config.yml /usr/share/spaghettikart/meta /usr/share/spaghettikart/yamls ./AppDir/shared/bin/
+mv /usr/bin/spaghetti.o2r /usr/bin/config.yml /usr/bin/meta /usr/bin/yamls ./AppDir/shared/bin/
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}' >> ./AppDir/.env
 
 # Additional changes can be done in between here
