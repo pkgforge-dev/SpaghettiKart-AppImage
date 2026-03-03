73f747c97e987d6b1ebd0ba03e00558f60b3032d
Link4Electronics <link4electronics@gmail.com>
2026-03-03 00:03:29 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,6 +8,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/spaghettikart.png
+export STARTUPWMCLASS=Spaghettify
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
